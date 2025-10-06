1404d6b36cbcc70342d5e4fa8591db57757f1456
Chugani05 <rashi.chugani.n@gmail.com>
2025-10-07 00:30:29 +0100

    start keybord style

--- src/index.php
+++ src/index.php
@@ -14,7 +14,7 @@ session_start();
 $storage = new Storage();
 $savedGame = $storage->get("saved_game");
 
-$wordProvider = new WordProvider("./files/words.txt");
+$wordProvider = new WordProvider("./data/words.txt");
 $game = new Game($wordProvider->randomWord(), state: $savedGame);
 
 if (isset($_POST['letra'])) {
@@ -39,6 +39,7 @@ $storage->set("saved_game", $game->toState());
 <head>
     <meta charset="UTF-8">
     <title>Hangman with PHP</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
 <h1>Hangman Game</h1>
@@ -50,14 +51,34 @@ $storage->set("saved_game", $game->toState());
 <p>Used letters: <?php echo implode(", ", $game->getUsedLetters()); ?></p>
 
 <?php if ($mensaje == ""): ?>
-    <form method="post">
-        <label>Enter a letter:</label>
-        <input type="text" name="letra" maxlength="1" required>
-        <button type="submit">Guess</button>
+    <form id="keyboardForm" method="post">
+        <div class="keyboard">
+            <?php foreach (range('A', 'Z') as $letra): ?>
+                <button type="submit" class="key" name="letra" value="<?php echo $letra; ?>"><?php echo $letra; ?></button>
+            <?php endforeach; ?>
+        </div>
     </form>
+
+    <script>
+        const usedLetters = <?php echo json_encode($game->getUsedLetters()); ?>;
+        const correctLetters = <?php echo json_encode(array_intersect($game->getUsedLetters(), str_split($game->getWord()))); ?>;
+        
+        document.querySelectorAll('.key').forEach(key => {
+            const letter = key.value;
+            if (usedLetters.includes(letter)) {
+                if (correctLetters.includes(letter)) {
+                    key.classList.add('correct');
+                } else {
+                    key.classList.add('incorrect');
+                }
+                key.disabled = true;
+            }
+        });
+    </script>
+
 <?php else: ?>
     <p><strong><?php echo $mensaje; ?></strong></p>
-    <a href="reset.php">Play again</a>
+    <a href="reset.php" class="play-again">Play again</a>
 <?php endif; ?>
 
 </body>
